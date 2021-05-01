7caad59d1bbebde7447c2944ac595718c17038ee
Michał Kosior <michal.kosior@vavatech.pl>
2021-05-01 13:59:33 +0200

    implement transferFrom function

--- smartcontract/contracts/Kittycontract.sol
+++ smartcontract/contracts/Kittycontract.sol
@@ -49,9 +49,44 @@ contract Kittycontract is IERC721, Ownable {
         _transfer(msg.sender, to, tokenId);
     }
 
+    /// @notice Transfers the ownership of an NFT from one address to another address
+    /// @dev Throws unless `msg.sender` is the current owner, an authorized
+    ///  operator, or the approved address for this NFT. Throws if `_from` is
+    ///  not the current owner. Throws if `_to` is the zero address. Throws if
+    ///  `_tokenId` is not a valid NFT. When transfer is complete, this function
+    ///  checks if `_to` is a smart contract (code size > 0). If so, it calls
+    ///  `onERC721Received` on `_to` and throws if the return value is not
+    ///  `bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"))`.
+    /// @param _from The current owner of the NFT
+    /// @param _to The new owner
+    /// @param _tokenId The NFT to transfer
+    /// @param data Additional data with no specified format, sent in call to `_to`
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes calldata data) override external {
+
+    }
+
+    /// @notice Transfers the ownership of an NFT from one address to another address
+    /// @dev This works identically to the other function with an extra data parameter,
+    ///  except this function just sets data to "".
+    /// @param _from The current owner of the NFT
+    /// @param _to The new owner
+    /// @param _tokenId The NFT to transfer
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId) override external {
+
+    }
+
+    function transferFrom(address _from, address _to, uint256 _tokenId) override external {
+        require(_tokenId < kitties.length, "Token doesn't exist");
+        require(_isApproved(msg.sender, _tokenId), "You are not authorize to transfer this token");
+        require(_owns(_from, _tokenId), "_from address isn't current owner");
+        require(_to != address(0), "_to address is 0 address");
+
+        _transfer(_from, _to, _tokenId)
+    }
+
     function approve(address _approved, uint256 _tokenId) override external {
         require(_tokenId < kitties.length, "Token doesn't exist");
-        require(_owns(msg.sender, _tokenId) || approvalsForAll[kittyOwner[_tokenId]][msg.sender], "You don't own this token");
+        require(_owns(msg.sender, _tokenId), "You don't own this token");
 
         approvals[_tokenId] = _approved;
 
@@ -59,6 +94,8 @@ contract Kittycontract is IERC721, Ownable {
     }
 
     function setApprovalForAll(address _operator, bool _approved) override external {
+        require(msg.sender != _operator);
+
         approvalsForAll[msg.sender][_operator] = _approved;
 
         emit ApprovalForAll(msg.sender, _operator, _approved);
@@ -126,9 +163,16 @@ contract Kittycontract is IERC721, Ownable {
         balances[to] += 1;
         if (from != address(0)) {
             balances[msg.sender] -= 1;
+            delete approvals[tokenId];
         }
 
         emit Transfer(msg.sender, to, tokenId);
+    }
+
+    function _isApproved(address claimant, uint256 tokenId) internal view returns(bool) {
+        return _owns(claimant, tokenId) ||
+                approvals[tokenId] == claimant ||
+                approvalsForAll[kittyOwner[tokenId]][claimant];
     }
 
     function _owns(address claimant, uint256 tokenId) internal view returns(bool) {
